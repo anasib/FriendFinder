e97262c7b9588d3c5ac0439a6759da3746586400
anasib <atta.nasib@bytecubed.com>
2018-05-15 14:15:50 -0400

    fixed code

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [{
+
+        "name": "Charlize Theron",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/6/69/Charlize_Theron_%286998773375%29.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Halle Berry",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/0/0e/Halle_Berry_%2835732678650%29.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            2,
+            3,
+            4,
+            2,
+            2,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Mila Kunis",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/1/1a/Mila_Kunis_%287601681006%29.jpg",
+        "scores": [
+            3,
+            1,
+            3,
+            4,
+            3,
+            1,
+            3,
+            5,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "Angelina Jolie",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Angelina_Jolie_2010.jpg",
+        "scores": [
+            1,
+            1,
+            4,
+            4,
+            5,
+            5,
+            4,
+            1,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "Megan Fox",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/9/9f/Megan_Fox_2011.jpg",
+        "scores": [
+            2,
+            1,
+            4,
+            4,
+            5,
+            3,
+            3,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Katrina Kaif",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/f/fc/Katrina-Kaif.jpg",
+        "scores": [
+            5,
+            3,
+            4,
+            4,
+            5,
+            4,
+            4,
+            1,
+            4,
+            1
+        ]
+    }
+
+];
+
+module.exports = friends;
